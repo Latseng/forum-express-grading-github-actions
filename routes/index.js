4ab3c9e8f5a8e1d09af722da87cdd6740f8a62e5
Lala Tseng <z162614@gmail.com>
2025-05-12 14:07:48 +0800

    feat:add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// 新增，載入 controller
+const restController = require('../controllers/restaurant-controller')
+
+router.get('/restaurants', restController.getRestaurants)// 匹配條件多的路由寫在前面
+router.use('/', (req, res) => res.redirect('/restaurants'))// fallback 路由，當沒有匹配到路由時，會導向 /restaurants
 
 module.exports = router

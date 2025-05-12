13662d101ccf1e7b3ec5e8120491130b919b3aae
Lala Tseng <z162614@gmail.com>
2025-05-12 14:18:28 +0800

    feat:add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-// 新增，載入 controller
+// 載入 controller
 const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin') // 載入 admin.js
 
+router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)// 匹配條件多的路由寫在前面
 router.use('/', (req, res) => res.redirect('/restaurants'))// fallback 路由，當沒有匹配到路由時，會導向 /restaurants
 

d6692dc8a75948563e9d0af22c0b7fc91699d309
Lala Tseng <z162614@gmail.com>
2025-05-13 15:05:38 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const passport = require('../config/passport')
 const admin = require('./modules/admin') // 載入 admin.js
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler') // 錯誤處理
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
@@ -20,7 +21,7 @@ router.post(
   userController.signIn
 )
 router.get('/logout', userController.logout)
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))// fallback 路由，當沒有匹配到路由時，會導向 /restaurants
 router.use('/', generalErrorHandler)
 

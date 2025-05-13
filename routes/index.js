feec9f257f3fb6c5e83b782044d0e2018bfbc8a5
Lala Tseng <z162614@gmail.com>
2025-05-13 14:10:36 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 
 // 載入 controller，匹配條件多的路由放在前面
-
 const admin = require('./modules/admin') // 載入 admin.js
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
@@ -10,6 +10,16 @@ const { generalErrorHandler } = require('../middleware/error-handler') // 錯誤
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post(
+  '/signin',
+  passport.authenticate('local', {
+    failureRedirect: '/signin',
+    failureFlash: true
+  }),
+  userController.signIn
+)
+router.get('/logout', userController.logout)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))// fallback 路由，當沒有匹配到路由時，會導向 /restaurants
 router.use('/', generalErrorHandler)

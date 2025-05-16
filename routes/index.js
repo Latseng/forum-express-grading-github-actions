ce6a86ff1e5c45ed79496e515a353ba3011f7b89
Lala Tseng <z162614@gmail.com>
2025-05-16 11:34:16 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ const passport = require('../config/passport')
 const admin = require('./modules/admin') // 載入 admin.js
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler') // 錯誤處理
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)

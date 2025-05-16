480639b3c0f1cc201c830e0a2072418d0fa2632a
Lala Tseng <z162614@gmail.com>
2025-05-16 12:24:17 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,7 +18,8 @@ app.use(express.urlencoded({ extended: true })) // 使用Express內建的body-pa
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize()) // 初始化 Passport
 app.use(passport.session()) // 啟動 session 功能
-app.use(flash()) // 掛載套件
+app.use(flash())
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息

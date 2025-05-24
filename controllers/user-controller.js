9e62e51c40c9b44c86651ddddb8527bbb4220971
Lala Tseng <z162614@gmail.com>
2025-05-24 10:52:28 +0800

    feat: create user profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs') // 載入 bcrypt
 const db = require('../models')
 const { User } = db
+const { localFileHandler } = require('../helpers/file-helpers')
+
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -10,21 +12,22 @@ const userController = {
     if (req.body.password !== req.body.passwordCheck) {
       throw new Error('Passwords do not match!')
     }
-
-    // 確認資料裡面沒有一樣的 email，若有，就建立一個 Error 物件並拋出
-    User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10)
-      })
-      .then(hash =>
+    const { file } = req // 將註冊時上傳的照片檔取出
+    Promise.all([
+      User.findOne({ where: { email: req.body.email } }), // 確認資料裡面沒有一樣的 email，若有，就建立一個 Error 物件並拋出
+      localFileHandler(file),
+      bcrypt.hash(req.body.password, 10)
+    ])
+      .then(([email, filePath, password]) => {
+        if (email) throw new Error('Email already exists!')
         User.create({
           // 上面錯誤狀況都沒發生，就把使用者的資料寫入資料庫
           name: req.body.name,
           email: req.body.email,
-          password: hash
+          password: password,
+          image: filePath || null
         })
-      )
+      })
       .then(() => {
         req.flash('success_messages', '成功註冊帳號！') // 並顯示成功訊息
         res.redirect('/signin')
@@ -42,6 +45,49 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      // raw: true,
+      // nest: true
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
+        res.render('users/profile', { user: user.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true }).then(user => {
+      if (!user) throw new Error("User didn't exist!")
+      res.render('users/edit', { user })
+    })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    const userId = req.user.id // 辨別發出請求的使用者id
+    if (!name) throw new Error('name is required!')
+    const { file } = req // 把檔案取出來
+    return Promise.all([
+      // 非同步處理
+      User.findByPk(userId), // 核對資料庫，確定只有使用者本人才能修改本人的資料
+      localFileHandler(file) // 把檔案傳到 file-helper 處理
+    ])
+      .then(([user, filePath]) => {
+        // 以上兩樣事都做完以後
+        if (!user) throw new Error("User didn't exist!")
+        return user.update({
+          // 修改這筆資料
+          name,
+          image: filePath || user.image // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${userId}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,11 @@ const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler') // 錯誤處理
+const upload = require('../middleware/multer')
 
 router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
-router.post('/signup', userController.signUp)
+router.post('/signup', upload.single('image'), userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post(
   '/signin',
@@ -32,6 +33,9 @@ router.delete(
   commentController.deleteComment
 )
 router.post('/comments', authenticated, commentController.postComment)
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+router.get('/users/:id', authenticated, userController.getUser)
 router.use('/', (req, res) => res.redirect('/restaurants'))// fallback 路由，當沒有匹配到路由時，會導向 /restaurants
 router.use('/', generalErrorHandler)
 
